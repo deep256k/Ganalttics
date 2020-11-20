98d7e87e461367db791058240c9908900d8f5ce2
Deepak <Deepak.kumar@moveinsync.com>
2020-11-20 15:30:56 +0530

    image change

--- src/app/analytics.service.ts
+++ src/app/analytics.service.ts
@@ -14,10 +14,9 @@ export class AnalyticsService {
                       eventLabel: string = null,
                       eventValue: number = null) {
 ga('send', 'event', {
-eventCategory: eventCategory,
-eventLabel: eventLabel,
-eventAction: eventAction,
-eventValue: eventValue
+eventCategory,
+eventLabel,
+eventValue
 });
  }
 }
